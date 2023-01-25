8f9ed2ba31eed3b490d7b278467263d83293d51f
YUJOO-LEE <lllllllllee@gmail.com>
2023-01-25 21:57:46 +0900

    disabled 버튼 스타일링

--- renderer/components/common/Button.tsx
+++ renderer/components/common/Button.tsx
@@ -13,12 +13,41 @@ export default Button;
 
 const Styled = {
   Button: styled.button`
+    position: relative;
     font-size: 14px;
     padding: 5px 10px;
     border: none;
     border-radius: 5px;
     background-color: transparent;
     cursor: pointer;
+
+    &:disabled{
+      text-indent: -99999px;
+      opacity: 0.5;
+
+      &::after{
+        content: '';
+        display: block;
+        position: absolute;
+        top: 8px;
+        left: 130px;
+        height: 20px;
+        width: 20px;
+        border-radius: 50%;
+        border: 2px solid rgba(255,255,255,.3);
+        border-top-color: #fff;
+        animation: rotate-loading 1.5s linear 0s infinite normal;
+      }
+
+      @keyframes loading {
+        0%  {
+          transform: rotate(0deg);
+        }
+        100% {
+          transform: rotate(360deg);
+        }
+      }
+    }
 
     &.big{
       padding: 10px;
