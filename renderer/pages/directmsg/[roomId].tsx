ca2e3b6ec7d087b0cd091bb9c08d583f6956b120
YUJOO-LEE <lllllllllee@gmail.com>
2023-01-29 03:08:24 +0900

    쿼리스트링으로 방 전체 ID를 받도록 수정

---
+++ renderer/pages/directmsg/[roomId].tsx
@@ -0,0 +1,39 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import MessageInput from '../../components/common/MessageInput';
+import { getChatRoomData } from '../../firebase/realtimeDB';
+import { ChatRoomType } from '../../types/chatRoom';
+
+const DmPage = () => {
+
+  const router = useRouter();
+  const { roomId } = router.query;
+  const stringRoomId = Array.isArray(roomId) ? roomId[0] : roomId;
+
+  const [DmRoomData, setDmRoomData] = useState<ChatRoomType>(null);
+
+  useEffect(() => {
+    getChatRoomData(stringRoomId, setDmRoomData, true);
+  }, []);
+
+  console.log(DmRoomData);
+
+  return (
+    <Styled.Wrapper>
+      <MessageInput roomId={stringRoomId} />
+    </Styled.Wrapper>
+  )
+}
+
+export default DmPage;
+
+const Styled = {
+  Wrapper: styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  `,
+}
