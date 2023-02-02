3e75ddb6d0119c3eae1c2f42306fbe185e74ac79
YUJOO-LEE <lllllllllee@gmail.com>
2023-02-02 22:06:42 +0900

    styled-components ssr시 적용되도록 컴파일러 모듈 추가

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -7,7 +7,10 @@ module.exports = {
       test: /\.(png|svg|jpg|jpeg|gif)$/i,
       type: 'asset/resource',
     });
-
+  
     return config;
   },
+  compiler: {
+    styledComponents: true,
+  }
 };
